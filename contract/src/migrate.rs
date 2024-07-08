a5d347646af5e9cb3cb67b93065956682951a5e5
akorchyn <artur.yurii.korchynskyi@gmail.com>
2024-07-08 19:50:12 +0300

    edited init.json

--- contract/src/migrate.rs
+++ contract/src/migrate.rs
@@ -1,6 +1,21 @@
-use near_sdk::near;
+use near_sdk::near_bindgen;
 
 use super::*;
 
-#[near]
-impl Contract {}
+#[near_bindgen]
+impl Contract {
+    #[init(ignore_state)]
+    #[private]
+    pub fn migrate(allowed_repos: Vec<AllowedRepos>) -> Self {
+        let mut contract: Contract = near_sdk::env::state_read().unwrap();
+
+        contract.organizations.clear();
+        for org in allowed_repos {
+            for repo in org.repos {
+                contract.include_repo(org.organization.clone(), repo)
+            }
+        }
+
+        contract
+    }
+}
