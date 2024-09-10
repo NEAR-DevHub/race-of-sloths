7b636c185d515b52f908199b5af82ca6cefe823d
akorchyn <artur.yurii.korchynskyi@gmail.com>
2024-09-10 16:29:59 +0300

    fix: do not update repo metadata for paused repos

--- server/sql/get_repos.sql
+++ server/sql/get_repos.sql
@@ -5,5 +5,7 @@ select
     o.full_name as organization_full_name
 from
     repos as r
-    JOIN organizations o ON r.organization_id = o.id for
+    JOIN organizations o ON r.organization_id = o.id
+where
+    r.paused = false for
 update
